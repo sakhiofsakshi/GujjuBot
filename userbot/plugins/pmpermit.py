b184c41208d974621a0fb54e674a193161a6b2c3
sakhiofsakshi <66856959+sakhiofsakshi@users.noreply.github.com>
2020-06-15 14:53:19 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -15,7 +15,7 @@ USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=948408212)\n\n"
                     "`Hello, this is Protected Security Service.You have found your way here to my master,`"
                     f"{DEFAULTUSER}`'s inbox.\n\n"
-                    "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
+                    "hi i am sakhi of sakshi wait for a while she will reply you soon dont message countinously otherwise block you.`\n\n"
                     "** Send** `/start` ** so that we can decide why you're here.**")
 
 
